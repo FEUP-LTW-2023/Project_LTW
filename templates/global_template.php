f9bc63290776a3c379c245d003832be4e47e83d1
Pedro Gomes <up202108825@up.pt>
2023-05-04 01:44:52 +0100

    global template and index.php done

---
+++ templates/global_template.php
@@ -0,0 +1,42 @@
+<?php function draw_topbar() { ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=100.0">
+
+    <link href="/../style/style.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="/../js/faq.js"></script>
+
+    <title>Frequently Asked Questions</title>
+</head>
+<body>
+<div id = "top-bar">
+    <nav id="menu">
+        <input type="checkbox" id="hamburger">
+        <label class="hamburger" for="hamburger"></label>
+        <ul>
+            <li><a href="agent-dashboard.html">Ticket dashboard</a></li>
+            <li><a href="/../pages/faq.php">Frequently asked questions</a></li>
+            <li><a href="">Performance metrics</a></li>
+            <li><a href="">Options</a></li>
+        </ul>
+    </nav>
+    <span id="username">
+        pedrofvgomes
+    </span>
+    <img class="avatar" src="../images/user.png">
+</div>
+
+<?php } ?>
+
+
+<?php function draw_footer() { ?>
+
+</body>
+</html>
+
+<?php } ?>
