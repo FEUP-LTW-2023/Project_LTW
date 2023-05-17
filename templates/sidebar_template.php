a3f7f523d8bc1d99d17df4e2f46b48a1447ff6bd
Pedro Gomes <up202108825@up.pt>
2023-05-17 23:27:00 +0100

    admin settings page done

---
+++ templates/sidebar_template.php
@@ -0,0 +1,99 @@
+<?php function draw_client_sidebar() { ?>
+    <section id="sidebar">
+		<a href="#" class="brand">
+			<img src="../new/assets/logoo.png" alt="Logo">
+			<span class="text">QUICKFIX</span>
+		</a>
+		
+		<ul class="side-menu top">
+			<li class="active">
+				<a href="#">
+					<i class='bx bxs-category' ></i>
+					<span class="text">Ticket Central</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bx-file' ></i>
+					<span class="text">Assigned Tickets</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-help-circle' ></i>
+					<span class="text">FAQ</span>
+				</a>
+			</li>
+		</ul>
+        <ul class="side-menu top">
+			<li>
+				<a href="#">
+					<i class='bx bxs-group' ></i>
+					<span class="text">User Roles</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-building' ></i>
+					<span class="text">Departments</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-cog' ></i>
+					<span class="text">Administration</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+<?php } ?>
+
+<?php function draw_agent_sidebar() { ?>
+    <section id="sidebar">
+		<a href="#" class="brand">
+			<img src="../assets/logoo.png" alt="Logo">
+			<span class="text">QUICKFIX</span>
+		</a>
+		
+		<ul class="side-menu top">
+			<li class="active">
+				<a href="#">
+					<i class='bx bxs-dashboard' ></i>
+					<span class="text">Ticket Central</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-doughnut-chart' ></i>
+					<span class="text">Assigned Tickets</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-message-dots' ></i>
+					<span class="text">FAQ</span>
+				</a>
+			</li>
+		</ul>
+        <ul class="side-menu top">
+			<li>
+				<a href="#">
+					<i class='bx bxs-group' ></i>
+					<span class="text">User Roles</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-building' ></i>
+					<span class="text">Departments</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class='bx bxs-cog' ></i>
+					<span class="text">Administration</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+<?php } ?>
