02487efadf447e4d41f95435eb2d0abef921ffc2
Pedro Gomes <up202108825@up.pt>
2023-05-18 15:50:38 +0100

    upgrade page done

---
+++ templates/upgrade_template.php
@@ -0,0 +1,94 @@
+<?php function draw_upgrade() { ?>
+    <section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu' ></i>
+			<form action="#">
+				
+			</form>
+			<label></label>
+			<a href="#" class="profile">
+				<img src="../new/img/people.png">
+			</a>
+		</nav>
+		<!-- NAVBAR -->
+
+		<!-- MAIN -->
+		<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>User Roles</h1>
+				</div>
+			</div>
+			<div class="table-data">
+				<div class="order">
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Email</th>
+								<th>Role</th>
+                                <th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="../new/img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><span class="status completed">Agent</span></td>
+                                <td><i class='bx bx-pencil' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="../new/img/people.png">
+									<p>Jane Smith</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><span class="status pending">Admin</span></td>
+                                <td><i class='bx bx-pencil' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="../new/img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><span class="status process">Client</span></td>
+                                <td><i class='bx bx-pencil' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="../new/img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><span class="status pending">Admin</span></td>
+                                <td><i class='bx bx-pencil' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="../new/img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><span class="status completed">Agent</span></td>
+                                <td><i class='bx bx-pencil' ></i></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				
+			</div>
+		</main>
+		<!-- MAIN -->
+	</section>
+	<!-- CONTENT -->
+	
+
+	<script src="../new/script.js"></script>
+</body>
+</html>
+<?php } ?>
