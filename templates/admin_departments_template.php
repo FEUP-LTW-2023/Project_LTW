d887117102496f85c7aa2806feb726b5ce22ffff
acarolinacc <acarolinacc2003@gmail.com>
2023-05-20 23:55:49 +0100

    admin department php

---
+++ templates/admin_departments_template.php
@@ -0,0 +1,266 @@
+<?php function draw_admin_departments () { ?>
+    <section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu' ></i>
+			<form action="#">
+				
+			</form>
+			<label></label>
+			<a href="#" class="profile">
+				<img src="img/people.png">
+			</a>
+		</nav>
+		<!-- NAVBAR -->
+
+		<!-- MAIN -->
+		<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Departments</h1>
+				</div>
+			</div>
+			<div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Technical Support</h3>
+						<i class='bx bx-plus' ></i>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Email</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				
+			</div>
+            <div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Account Management</h3>
+						<i class='bx bx-plus' ></i>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Email</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				
+			</div>
+            <div class="table-data">
+				<div class="order">
+					<div class="head">
+						<h3>Human Resources</h3>
+						<i class='bx bx-plus' ></i>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>User</th>
+								<th>Email</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+							<tr>
+								<td>
+									<img src="img/people.png">
+									<p>John Doe</p>
+								</td>
+								<td>johndoe1234@gmail.com</td>
+								<td><i class='bx bx-trash' ></i></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="table-data">
+                    <div class="order">
+                        <div class="head">
+                            <h3>Operations and Logistics</h3>
+                            <i class='bx bx-plus' ></i>
+                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>User</th>
+                                    <th>Email</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img src="img/people.png">
+                                        <p>John Doe</p>
+                                    </td>
+                                    <td>johndoe1234@gmail.com</td>
+                                    <td><i class='bx bx-trash' ></i></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="img/people.png">
+                                        <p>John Doe</p>
+                                    </td>
+                                    <td>johndoe1234@gmail.com</td>
+                                    <td><i class='bx bx-trash' ></i></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="img/people.png">
+                                        <p>John Doe</p>
+                                    </td>
+                                    <td>johndoe1234@gmail.com</td>
+                                    <td><i class='bx bx-trash' ></i></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="img/people.png">
+                                        <p>John Doe</p>
+                                    </td>
+                                    <td>johndoe1234@gmail.com</td>
+                                    <td><i class='bx bx-trash' ></i></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="img/people.png">
+                                        <p>John Doe</p>
+                                    </td>
+                                    <td>johndoe1234@gmail.com</td>
+                                    <td><i class='bx bx-trash' ></i></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+			</div>
+		</main>
+		<!-- MAIN -->
+	</section>
+    <script src="../new/script.js"></script>
+</body>
+</html>
+<?php } ?>
