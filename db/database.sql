a6dd4e809588e1df8a1348d4efbb8c0bfaa085ed
Pedro Gomes <up202108825@up.pt>
2023-05-20 15:17:43 +0100

    database connection

--- db/database.sql
+++ db/database.sql
@@ -4,17 +4,17 @@ drop table if exists Ticket;
 
 create table Account
 (
-    id integer primary key not null autoincrement,
+    id integer primary key autoincrement not null,
     username varchar(255) not null unique,
     email varchar(255) not null unique,
     password nvarchar(255) not null,
-    name varchar(255) not null,
-    role varchar(255) not null,
+    name varchar(255),
+    role varchar(255)
 );
 
 create table Ticket
 (
-    id integer primary key not null autoincrement,
+    id integer primary key autoincrement not null,
     author integer not null references Account(id),
     agent integer references Account(id),
     title varchar(255),
