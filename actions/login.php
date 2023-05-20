453ad95f6049e2733daa074a747813d7bbc9719f
Pedro Gomes <up202108825@up.pt>
2023-05-20 11:34:19 +0100

    added login and logout actions

---
+++ actions/login.php
@@ -0,0 +1,26 @@
+<?php
+    declare(strict_types=1);
+
+    require_once(__DIR__ . '/../session.php');
+    $session = new Session();
+
+    require_once(__DIR__ . '/../db/connection.php');
+    require_once(__DIR__ . '/../db/account_class.php');
+
+    $db = getdbconnection();
+
+    $user = Account::login($db, $_POST['usernameemail'], $_POST['password']);
+
+    if($user){
+        $session->setId($user->id);
+        $session->setUsername($user->username);
+        $session->setEmail($user->email);
+        $session->setName($user->name);
+        $session->setRole($user->role);
+        $session->addMessage('success', 'Login successful!');
+    }
+    else $session->addMessage('error', 'Email/username or password wrong.');
+
+    header('Location: '. $_SERVER['HTTP_REFERER']);
+
+?>
