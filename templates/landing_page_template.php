7ccb9ee1ca5cb8f6c48430b820684c451b0a57bc
acarolinacc <acarolinacc2003@gmail.com>
2023-05-20 23:57:45 +0100

    landing page php

---
+++ templates/landing_page_template.php
@@ -0,0 +1,22 @@
+<?php function draw_lading_page () { ?>
+    <div class="hero-image"></div>
+    <div class="main-wrapper">
+      <header class="header">
+        <img class="logo" src="assets/logo-no-background.png" alt="logo" />
+      </header>
+      <main>
+        <section class="intro">
+          <div class="intro-text">
+            <h1 class="intro-heading">Peaceful Troubleshooting.</h1>
+            <p class="intro-paragraph">
+              Efficiently resolve and track your tickets with our streamlined and user-friendly website. Experience hassle-free ticket submission, real-time updates, and insightful reporting functionalities, all in one place. Whether you're a client, agent, or admin, our system provides the tools you need to manage tickets promptly and effectively.
+            </p>
+            <button class="intro-btn">Get Started</button>
+          </div>
+        </section>
+      </main>
+    </div>
+    <script src=".../new/script.js"></script>
+</body>
+</html>
+<?php } ?>
